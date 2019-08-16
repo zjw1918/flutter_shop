66049fcc2fb690a0827514253bd76a35fdcbd276
Zhou <zjw1918@sina.cn>
2019-08-16 23:44:54 +0800

    轮播图ok; FutureBuilder了解;

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-import 'package:shop/configs/http_headers.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -19,32 +17,61 @@ class _HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text('Home'),
         ),
-        body: SingleChildScrollView(
-          child: Column(children: <Widget>[
-            RaisedButton(
-              onPressed: _goGet,
-              child: Text('go get'),
-            ),
-            Text(showTest)
-          ],),
+        body: FutureBuilder(
+          future: fetch(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final data = snapshot.data['results'];
+              return Column(
+                children: <Widget>[
+                  SwiperDiy(list: data,)
+                ],
+              );
+            } else {
+              return Container(
+                child: Text('no data'),
+              );
+            }
+          },
         ),
       ),
     );
   }
 
-  void _goGet() {
-    fetch().then((v) {
-      setState(() {
-        showTest = v['data'].toString();
-      });
-    })
-    .catchError((e) => print(e));
-  }
+  // void _goGet() {
+  //   fetch().then((v) {
+  //     setState(() {
+  //       showTest = v['data'].toString();
+  //     });
+  //   })
+  //   .catchError((e) => print(e));
+  // }
 
   Future fetch() async {
     var client = Dio();
-    client.options.headers = HTTP_HEADERS;
-    var res = await client.get('https://time.geekbang.org/serv/v1/column/topList');
+    var res = await client.get('https://randomuser.me/api/?results=5&&gender=female');
     return res.data;
   }
 }
+
+class SwiperDiy extends StatelessWidget {
+  final List list;
+
+  SwiperDiy({Key key, this.list}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333,
+      child: Swiper(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return Image.network(list[index]['picture']['large'], fit: BoxFit.fill,);
+        },
+        autoplay: true,
+        pagination: SwiperPagination(),
+      ),
+    );
+  }
+}
+
