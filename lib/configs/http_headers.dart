53242cf2a722054a9af0c2dc54ffae7a251b29cd
Zhou <zjw1918@sina.cn>
2019-08-16 19:37:33 +0800

    ...

---
+++ lib/configs/http_headers.dart
@@ -0,0 +1,14 @@
+const HTTP_HEADERS = {
+  'Cookie': '_ga=GA1.2.1326153160.1565945625; _gid=GA1.2.1223155046.1565945625; _gat=1; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1565945626; SERVERID=1fa1f330efedec1559b3abbcb6e30f50|1565945643|1565945624; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1565945644',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,ja;q=0.6',
+  'Connection': 'keep-alive',
+  'DNT': '1',
+  'Host': 'time.geekbang.org',
+  'Origin': 'https://time.geekbang.org',
+  'Referer': 'https://time.geekbang.org/',
+  'Sec-Fetch-Mode': 'cors',
+  'Sec-Fetch-Site': 'same-origin',
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:shop/configs/http_headers.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,68 +10,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String showText = 'Welcome';
-  TextEditingController editingController = TextEditingController();
+  var showTest = 'Welcome...';
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        appBar: AppBar(title: Text('Home'),),
-        // SingleChildScrollView 防止键盘弹出导致尺寸溢出
-        body: SingleChildScrollView(child: Container(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: editingController,
-                decoration: InputDecoration(
-                  labelText: '美女类型',
-                  prefixIcon: Icon(Icons.account_box, size: 28.0,),
-                  helperText: '请输入喜欢的类型',
-                  contentPadding: EdgeInsets.all(10.0),
-                  suffixIcon: IconButton(icon: Icon(Icons.close), onPressed: () => editingController.clear(),)
-                ),
-                autofocus: false, // 防止键盘自动弹出
-              ),
-              RaisedButton(
-                onPressed: _chooseAction,
-                child: Text('choosed done.'),
-              ),
-              Text(showText, overflow: TextOverflow.ellipsis, maxLines: 1,)
-            ],
-          ),
-        ),)
-
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            RaisedButton(
+              onPressed: _goGet,
+              child: Text('go get'),
+            ),
+            Text(showTest)
+          ],),
+        ),
       ),
     );
   }
 
-  void _chooseAction() {
-    print('check chosed info...');
-    if (editingController.text.toString() == '') {
-      showDialog(
-        context: context, 
-        builder: (context) => AlertDialog(title: Text('不能为空'),)
-      );
-    } else {
-      getHttp(editingController.text.toString())
-      .then((v) {
-        setState(() {
-          showText = v['data']['name'];
-        });
-      })
-      .catchError((e) {
-        print(e);
-        Scaffold.of(context).showSnackBar(SnackBar(content: Text('net error'),));
+  void _goGet() {
+    fetch().then((v) {
+      setState(() {
+        showTest = v['data'].toString();
       });
-    }
+    })
+    .catchError((e) => print(e));
   }
 
-  Future getHttp(String input) async {
-    final data = {'name': input};
-    var res = await Dio().get('https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian', 
-      queryParameters: data,
-    );
+  Future fetch() async {
+    var client = Dio();
+    client.options.headers = HTTP_HEADERS;
+    var res = await client.get('https://time.geekbang.org/serv/v1/column/topList');
     return res.data;
   }
 }
